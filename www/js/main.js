c6e7491d24ed5c179d5f217cce6d3ea0d98c474f
Bhabani Mishra <bmishra@ptc.com>
2015-10-28 18:07:37 +0530

    Added method to find cards by class and mana cost. Added a new css file for styling the pages.

--- www/js/main.js
+++ www/js/main.js
@@ -3,9 +3,31 @@ $.getJSON( "data/all-cards-local.json", function( data ) {
     $.each( data, function( cards ) {
         if(this instanceof Array)
         { 
-            $.each( this, function( id , card ) {                          
-                $('#cards').append('<img src='+card.image_url+' alt='+card.name+'/></br>');                
+            $.each( this, function( id , card ) {      
+                if(isPlayableByClass(this, "mage") && isManaCostBetween(this, 10, 0)  && isPlayableCard(card)) {                  
+                    if(card.hero == "neutral") {
+                        $('#neutral_cards').append('<img src='+card.image_url+' alt='+card.name+'/>'); 
+                    } else {
+                        $('#class_cards').append('<img src='+card.image_url+' alt='+card.name+'/>');                         
+                    }
+                    
+                }
             });
         }
     });
 });
+
+isPlayableCard = function(card) {
+  return (card.collectible == true && card.category!= "hero");
+}
+
+isPlayableByClass = function(card, hero) { 
+    /*if(card.name == "Voidwalker") {
+        console.log('wait');
+    }*/
+    return (card.hero == hero || card.hero == "neutral");    
+}
+
+isManaCostBetween = function(card, upperlimit, lowerlimit) {
+    return (card.mana <= upperlimit && card.mana >=lowerlimit);
+}
